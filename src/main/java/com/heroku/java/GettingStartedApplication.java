d6214b89e1b2f21fc1505d5b6fe2cb1683ab7af0
Peter Knipp <peteraknipp@gmail.com>
2024-10-16 17:32:15 -0400

    Add jacobi code lifted from "Numerical Recipes".

--- src/main/java/com/heroku/java/GettingStartedApplication.java
+++ src/main/java/com/heroku/java/GettingStartedApplication.java
@@ -34,6 +34,97 @@ public class GettingStartedApplication {
         return "result";
     }
 
+    public String jacobi(float [][] a) {
+        // Computes all eigenvalues and eigenvectors of a real symmetric matrix a, which is of size n by n, stored in a physical np by np array. On output, elements of a above the diagonal are
+        // destroyed. d returns the eigenvalues of a in its first n elements. v is a matrix with the same
+        // logical and physical dimensions as a, whose columns contain, on output, the normalized
+        // eigenvectors of a. nrot returns the number of Jacobi rotations that were required.
+        n = a.length;
+        int[][] v = new float[n][n];
+        int[][] b = new float[n];
+        int[][] d = new float[n];
+        int[][] z = new float[n]
+        for (ip = 0; ip < n; ip++) {
+            v[ip][ip] = 1;
+            b[ip] = a[ip][ip];
+            d[ip] = b[ip];
+            z[ip] = 0
+        }
+        nrot = 0;
+        for (int i = 0; i < 50; i++) {
+            sm = 0;
+            for (int ip = 0; ip < n - 1; ip++) {
+                for (int iq = ip + 1; iq < n; iq++) {
+                    sm += Math.abs(a[ip][iq]);
+                }
+            }
+            if (sm == 0) return "Algo ended well.";
+            if (i < 4) then {
+                tresh = 0.2 * sm / Math.pow(n, 2);
+            } else {
+                tresh = 0;
+            }
+            for (int ip = 0; ip < n - 1) {
+                for (int iq = ip + 1; iq < n; iq++) {
+                    g = 100 * Math.abs(a[ip][iq]);
+//                  After four sweeps, skip the rotation if the off-diagonal element is small.
+                    if (i > 3 && (Math.abs(d[ip]) + g == Math.abs(d[ip])) && (Math.abs(d[iq]) + g == Math.abs(d[iq]))) {
+                        a[ip][iq] = 0;
+                    } else if (Math.abs(a[ip][iq]) > tresh) {
+                        h = d[iq] - d[ip];
+                        if (Math.abs(h) + g == Math.abs(h)) {
+                            t = a[ip][iq] / h;
+                        } else {
+                            theta = 0.5 * h / a[ip][iq];
+                            t = 1 / (Math.abs(theta) + Math.sqrt(1 + Math.pow(theta, 2)));
+                            if (theta < 0) t = -t;
+                        }
+                        c = 1 / Math.sqrt(1 + Math.pow(t, 2));
+                        s = t * c;
+                        tau = s / (1 + c);
+                        h=t*a[ip][iq];
+                        z[ip] = z[ip] - h;
+                        z[iq] = z[iq] + h;
+                        d[ip] = d[ip] - h;
+                        d[iq] = d[iq] + h;
+                        a[ip][iq] = 0;
+                        for (int j = 0; j < ip - 1; j++) {
+                            g = a[j][ip];
+                            h = a[j][iq];
+                            a[j][ip] = g - s * (h + g * tau);
+                            a[j][iq] = h + s * (g - h * tau);
+                        }
+                        for (int j = ip + 1; j < iq - 1; j++) {
+                            g = a[ip][j];
+                            h = a[j][iq];
+                            a[ip][j] = g - s * (h + g * tau);
+                            a[j][iq] = h + s * (g - h * tau);
+                        }
+                        for (int j = iq + 1; j < n) {
+                            g = a[ip][j];
+                            h = a[iq][j];
+                            a[ip][j] = g - s * (h + g * tau);
+                            a[iq][j] = h + s * (g - h * tau);
+                        }
+                        for (int j = 0; j < n; j++) {
+                            g = v[j][ip];
+                            h = v[j][iq];
+                            v[j][ip] = g - s * (h + g * tau);
+                            v[j][iq] = h + s * (g - h * tau);
+                        }
+                        nrot++;
+                    }
+                }
+            }
+            for (int ip= 0; ip < n; ip++) {
+                b[ip] = b[ip] + z[ip];
+                d[ip] = b[ip];
+                z[ip] = 0;
+            }
+            return "Algo went thru too many iterations.";
+        }
+    }
+
     @GetMapping("/database")
     String database(Map<String, Object> model) {
         try (Connection connection = dataSource.getConnection()) {
